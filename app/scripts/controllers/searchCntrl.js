4555c3daa7cfa562cf831add1d825c57f6b542d5
sims <mahesh.budeti@smartims.com>
2015-06-26 17:44:06 +0530

    version4
    code to read the service url from cross domain, code to fill the
    dropdown values dynamically and changed code of date picker to jquery ui

--- app/scripts/controllers/searchCntrl.js
+++ app/scripts/controllers/searchCntrl.js
@@ -8,7 +8,19 @@
  * Controller of the appApp
  */
 angular.module('appApp')
-  .controller('searchCntrl', function ($scope, $rootScope) {
+  .controller('searchCntrl', function ($scope, $rootScope, utilityFactory) {
+        $('#fromdate').datepicker({
+        dateFormat: "yy-mm-dd",
+        changeMonth: true,
+        changeYear: true
+        });
+        $('#todate').datepicker({
+        dateFormat: "yy-mm-dd",
+        changeMonth: true,
+        changeYear: true
+        });
+     // TODO: need to generate dynamic url.
+    var searchUrl = "https://api.fda.gov/drug/event.json?search=receivedate:[2014-01-01+TO+2015-06-24]+AND+(BUPROPION)";
   	$scope.showSearchResults = false;
      if ($rootScope.showAdvSearch) { // Show advanced search open on load.
           $scope.isAdvSearchBlockOpen = true;
@@ -17,5 +29,9 @@ angular.module('appApp')
      }
      $scope.getSearchResults = function() {
      	$scope.showSearchResults = true;
+          utilityFactory.doCrossDomainGet(searchUrl);
+     };
+     $scope.renderSearchContent = function() {
+          // $scope.searchResult = 
      }
-  });
+});
